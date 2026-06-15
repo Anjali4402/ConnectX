6389228fef5fd69ee911b27891d3463f3acf5c47
Anjali <113291155+Anjali4402@users.noreply.github.com>
2026-06-15 21:43:40 +0530

    Project initiate

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-2xl">
+        Hello from <span className="font-bold">ConnectX</span>
+      </h1>
+    </div>
+  );
+}
