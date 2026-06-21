c4370a9f747ecc302eafe866e785548d73f929e7
Anjali <113291155+Anjali4402@users.noreply.github.com>
2026-06-21 21:47:36 +0530

    Added logo

--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -7,19 +7,32 @@ import {
 } from "@/components/ui/sidebar";
 import UserInfo from "./UserInfo";
 import Searchbar from "./Searchbar";
-import ChatInitiateBtn from "./ChatInitiateBtn";
+import CreateChatSection from "./CreateChatSection";
 import NavigationBar from "./NavigationBar";
 import ChatList from "./ChatList";
+import Image from "next/image";
 
 export function AppSidebar() {
   return (
     <Sidebar className=" backdrop-blur-2xl border-r border-white/5  w-105">
-      <SidebarHeader>
-        <h1>Side bar header</h1>
+      <SidebarHeader className="border-b border-white/5 mb-4 ">
+        <div className="flex items-center gap-2 ml-4 ">
+          <Image
+            src={"logo.svg"}
+            width={38}
+            height={38}
+            className="rounded-full border-2 border-primary/20 "
+            alt="Picture of the author"
+          />
+
+          <span className="text-lg text-primary tracking-tighter font-bold">
+            ConnectX
+          </span>
+        </div>
       </SidebarHeader>
 
       <div className="space-y-4 px-4 ">
-        <ChatInitiateBtn />
+        <CreateChatSection />
         {/* <!-- Filter/Navigation Bar --> */}
         <NavigationBar />
         {/* <!-- Search --> */}
