34382bd602c8da93f86dc3010c434de7984bce0f
Anjali <113291155+Anjali4402@users.noreply.github.com>
2026-06-24 21:31:59 +0530

    Added and setup Global-modal-layout

---
+++ client/src/components/ui/shared/ModalLayout.tsx
@@ -0,0 +1,22 @@
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Dispatch, ReactNode, SetStateAction } from "react";
+
+interface ModalLayoutProps {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  children: ReactNode;
+}
+
+export function ModalLayout({ open, setOpen, children }: ModalLayoutProps) {
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent
+        className=" w-full md:max-w-4xl bg-[#081425] rounded-[32px] [&>button]:hidden p-0
+      glass-morphism min-h-[60vh] h-fit  shadow-2xl  overflow-hidden"
+      >
+        {children}
+      </DialogContent>
+      <DialogTrigger />
+    </Dialog>
+  );
+}
