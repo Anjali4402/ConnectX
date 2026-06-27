63558c097f7452169017fcb73ad616cb571f5c0f
Anjali <113291155+Anjali4402@users.noreply.github.com>
2026-06-27 22:25:16 +0530

    Give access to all domain for temporary basis

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Matches any domain
+      },
+      {
+        protocol: "http",
+        hostname: "**", // Matches any non-secure domain if required
+      },
+    ],
+  },
 };
 
 export default nextConfig;
