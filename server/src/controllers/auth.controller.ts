981d7bccee1bc0fdcc8c4d2c821363206f0a3902
Anjali <113291155+Anjali4402@users.noreply.github.com>
2026-07-16 23:32:25 +0530

    Added signup fixes

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -69,7 +69,7 @@ async function loginController(req: Request, res: Response) {
 
     return res.status(201).json({
       success: true,
-      data: user,
+      data: null,
       message: "User Registered Successfully!",
     });
   } catch (error) {
@@ -92,95 +92,94 @@ async function loginController(req: Request, res: Response) {
   });
 }
 
-// async function registerController(req: Request, res: Response) {
-//   try {
-//     // Destructuring request data.
-//     const { username, email, password, bio, avatar } = req.body;
+async function registerController(req: Request, res: Response) {
+  try {
+    // Destructuring request data.
+    const { username, email, password, bio, avatar } = req.body;
 
-//     // Check weather user already exist or not. (based on email and username)
-//     const existingUser = await userModal.findOne({
-//       $or: [{ email }, { username }],
-//     });
+    // Check weather user already exist or not. (based on email and username)
+    const existingUser = await userModal.findOne({
+      $or: [{ email }, { username }],
+    });
 
-//     // If user already exist then return the message.
-//     if (existingUser) {
-//       return res.status(409).json({
-//         success: false,
-//         data: null,
-//         message:
-//           existingUser.email === email
-//             ? "Email is already registered."
-//             : "Username is already taken.",
-//       });
-//     }
+    // If user already exist then return the message.
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        data: null,
+        message:
+          existingUser.email === email
+            ? "Email is already registered."
+            : "Username is already taken.",
+      });
+    }
 
-//     // hash user password
-//     let hashedPassword;
-//     try {
-//       hashedPassword = await hashPassword(password);
-//     } catch (error) {
-//       return res.status(500).json({
-//         success: false,
-//         data: null,
-//         message: "Failed to process the request",
-//       });
-//     }
+    // hash user password
+    let hashedPassword;
+    try {
+      hashedPassword = await hashPassword(password);
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: "Failed to process the request",
+      });
+    }
 
-//     // Create User
-//     const user = await userModal.create({
-//       username,
-//       email,
-//       password: hashedPassword,
-//       bio,
-//       avatar,
-//     });
+    // Create User
+    const user = await userModal.create({
+      username,
+      email,
+      password: hashedPassword,
+      bio,
+      avatar,
+    });
 
-//     /// JWt added
-//     let token;
-//     try {
-//       token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-//         expiresIn: "3d",
-//       });
-//     } catch (error) {
-//       return res.status(500).json({
-//         success: false,
-//         data: null,
-//         message: "Failed to process the request",
-//       });
-//     }
+    /// JWt added
+    let token;
+    try {
+      token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "3d",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: "Failed to process the request",
+      });
+    }
 
-//     // send jwt token in the cookies
+    // send jwt token in the cookies
 
-//     res.cookie("token", token, {
-//       // sameSite: "strict",
-//       // secure: true,
-//       // httpOnly: true,
-//       maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-//     });
+    res.cookie("token", token, {
+      // sameSite: "strict",
+      // secure: true,
+      // httpOnly: true,
+      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+    });
 
-//     return res.status(201).json({
-//       success: true,
-//       data: user,
-//       message: "User Registered Successfully!",
-//     });
-//   } catch (error) {
-//     const firstError = Object.values(error.errors)[0];
+    return res.status(201).json({
+      success: true,
+      data: user,
+      message: "User Registered Successfully!",
+    });
+  } catch (error) {
+    const firstError = Object.values(error.errors)[0];
 
-//     if (error.name === "ValidationError") {
-//       return res.status(400).json({
-//         success: false,
-//         data: null,
-//         message: error.message.split(":")[0],
-//         error: firstError.message,
-//       });
-//     }
-//   }
+    if (error.name === "ValidationError") {
+      return res.status(400).json({
+        success: false,
+        data: null,
+        message: error.message.split(":")[0],
+        error: firstError.message,
+      });
+    }
+  }
 
-//   return res.status(500).json({
-//     success: false,
-//     message: "Internal Server Error",
-//   });
-// }
-async function registerController(req: Request, res: Response) {}
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+}
 
 export { loginController, registerController };
